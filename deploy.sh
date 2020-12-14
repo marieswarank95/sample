4bfd810256d9bf56c44c4fb7b36ff761137cf7b7
marieswarank <marieswarank@teezle.com>
2020-12-14 08:59:24 +0530

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh 
+ ssh ec2-user@10.0.0.229 <<EOF
+ mkdir test
+ cd test
+ git init
+ git remote add origin https://github.com/marieswarank95/sample.git
+ git pull origin master
+ npm install express
+ forever start app.js
+ exit
+EOF
